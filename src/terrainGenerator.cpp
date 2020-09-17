92a4890c38a443a71b40ae5b152a67081f825a79
cappelletto <cappelletto@gmail.com>
2020-09-17 15:46:38 +0100

    Add function and canvas structures

--- src/terrainGenerator.cpp
+++ src/terrainGenerator.cpp
@@ -10,8 +10,41 @@
  */
 #include "terrainGenerator.h"
 
-using std;
+using namespace std;
 
 namespace teg{
+    enum CanvasType{
+        depthMap = -1,  // the map is positive down (standard bathymetry)
+        heightMap = 1   // the map is positive up (standard altimetry)
+    };
+
+    enum WaveFormType{
+        constant = 1,   // fixed value y = D
+        step,           // unitary step response, y=1 if t>0
+        ramp,           // slope=1 ramp defined for t>0
+        pulse,          // square pulse defined for a given period 0<= t <= T, where T:1/B
+        square,         // periodic squared signal 
+        pwm,            // similar to square, but with user defined duty-cycle
+        triangular,     // periodic triangular signal
+        saw,            // saw-shaped periodic signal
+        sine            // pure sinusoidal
+    };
+
+    typedef struct{         // Waveform parameters as defined by template:
+                            // Y = A.func(B.x + C) + D : amplitude x func(frequency.t + phase) + offset 
+        WaveFormType type;           // waveform type
+        double amplitude;   // (A)   peak amplitude of the waveform
+        double period;      // (T:1/B) Spatial period of the wavefor if periodic. Signal duration if non periodic 
+        double phase;       // (C)   Spatial phase (offset)
+        double offset;      // (D)   Vertical offset (ordinate axis)
+        double frequency;   // (B)   Reciprocal of period
+    }fnParametersStruct;
+
+    typedef struct{
+        int mode;           // +1 Height, -1 Depth
+        double resolution;  // meter per pixel
+        double rotation;    // default : 0
+        double nodatafield; // for geoTIFF export
+    }canvasParametersStruct;
 
 };
